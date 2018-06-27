8517a2556a20b2d078f79ab554985d5a547f8ab0
Emilien <admin@emilienleroy.fr>
2018-06-27 09:11:39 +0200

    add: app component

---
+++ assets/Components/app.vue
@@ -0,0 +1,23 @@
+<template>
+<main>
+<h1>Swapi Project</h1>
+
+</main>
+</template>
+
+<script>
+export default {
+	name: 'app',
+	mounted: function(){
+		console.log("product");
+		this.$http.get("https://swapi.co/api/people/1/").then((response) =>{
+			console.log(response.body)
+		})
+	}
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,12 @@
-console.log("ok")
+import Vue from 'vue'
+import app from './Components/app.vue'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+new Vue({
+	mounted() {
+
+	}, 
+	components: { app }, 
+	template: "<app/>"
+}).$mount("#app")
